2a7d32a4cb95fc1f7a0616d32d387a1fafccc22e
fito <pp@dummy.eds>
2017-02-21 13:56:59 -0300

    First commit

---
+++ Tesis/tesis/internals.pl
@@ -0,0 +1,162 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_moffatt1981handbook/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Park2009/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Irwin2005/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/S:motivacion/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Zhai1995/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:tautes/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/S:tes/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_secceff/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Smith2012/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_squires/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Nishikawa2008/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Irwin1995/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_knoll/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tecnica/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Miki2006/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Mccammon2005/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Hilton1998/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Andrews1949/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Bardeen1957/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Vaglio2002/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/S:super/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:eficaz/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Buzea2001/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:est/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Naito2004/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:enertes/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/S:mgb2/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Saito2002/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Vinod2010/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:reaccion/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Andrews1942/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/S:crecimiento/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Satoh2003/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Barrabas2011/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Nagamatsu2001/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Ishida2007/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/S:objetivos/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Kim2003/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Xi2004/;
+$ref_files{$key} = "$dir".q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ Tesis/tesis/labels.pl
@@ -0,0 +1,173 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_Satoh2003/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Naito2004/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:est/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:reaccion/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Vinod2010/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Hilton1998/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_squires/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Xi2004/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Zhai1995/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/S:motivacion/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Miki2006/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tecnica/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:tautes/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/S:super/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/S:crecimiento/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Mccammon2005/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Ishida2007/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Andrews1949/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Irwin2005/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Bardeen1957/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Irwin1995/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/S:mgb2/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/S:objetivos/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Barrabas2011/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Park2009/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:eficaz/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Saito2002/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:enertes/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Kim2003/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Nishikawa2008/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Buzea2001/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/S:tes/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Vaglio2002/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_knoll/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Andrews1942/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_moffatt1981handbook/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Smith2012/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Nagamatsu2001/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_secceff/;
+$external_labels{$key} = "$URL/" . q|tesis.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2008 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/_/;
+$external_latex_labels{$key} = q|<|; 
+$noresave{$key} = "$nosave";
+
+1;
+
